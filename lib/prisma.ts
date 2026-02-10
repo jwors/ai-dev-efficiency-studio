843e716ea709cbbbec1cd22f1697567d9290395b
jwors <j.wors@outlook.com>
2026-02-10 14:49:59 +0800

    feat: 测试问题

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({connectionString})
 export function getPrisma() {
   if (globalForPrisma.prisma) return globalForPrisma.prisma;
 
   const client = new PrismaClient({
+    adapter,
     log: ["error", "warn"],
   });
 
